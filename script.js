34dba9f4a48defcc672c283f25140fada1927167
Igor Araújo <igoritoaraujo223@gmail.com>
2024-03-04 10:53:37 -0300

    Atualização arquivo Javascript

---
+++ script.js
@@ -0,0 +1,18 @@
+window.revelar = ScrollReveal({reset:true});
+
+
+revelar.reveal('.efeito-img-topo', {
+    duration: 2000,
+    distance: '90px'
+})
+
+
+revelar.reveal('.clientes-efeito', {
+    duration: 2000,
+    distance: '120px'
+})
+
+revelar.reveal('.principais-produtos', {
+    duration: 2000,
+    distance: '200px'
+})
