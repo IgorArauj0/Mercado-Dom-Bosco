ea90ed93cb3aa2f2f8402183817dd4fdff624ee1
Igor Araújo <igoritoaraujo223@gmail.com>
2024-10-07 19:25:51 -0300

    Salvamento

--- script.js
+++ script.js
@@ -16,3 +16,17 @@ revelar.reveal('.principais-produtos', {
     duration: 2000,
     distance: '200px'
 })
+
+
+
+
+//Aqui adicionamos um evento de escutar o click do Mouse sobre o ícone de Hamburguer.
+hamburgerButton.addEventListener("click", function() {
+    mobileMenu.classList.add("flex");
+    //Criamos uma função que a ouvir o click do Mouse, ele adicione uma Class, que está fazendo nossa caixa (Div) aparecer, ou seja, ficar visível.
+});
+
+//Criamos uma função que a ouvir o click do Mouse, ele REMOVA uma Class, que está fazendo nossa caixa (Div) desaparecer, ou seja, ficar invisível.
+closeButton.addEventListener("click", function() {
+    mobileMenu.classList.remove("flex");
+});
